2b7c6ed92eb82591dfb8c446ffa5a065524beddc
ilionash <heebum1216@naver.com>
2020-07-17 14:47:07 +0000

    #2.4 Extractin Indeed Pages part Two Get String from span tag, Get the largest number in pages

--- main.py
+++ main.py
@@ -24,17 +24,20 @@ indeed_soup = BeautifulSoup(indeed_result.text, 'html.parser')
 pagination = indeed_soup.find("div", {"class" : "pagination"})
 
 # "pagination" 변수에 저장된 html 중에서 "anchor" Tag 인 것 모두 가져와서 리스트로 저장
-pages = pagination.find_all("a")
+links = pagination.find_all("a")
 
 # span을 저장할 빈 리스트 객체 생성
-spans = []
+pages = []
 
-# "pages" 리스트에 저장된 값을 차례대로 "page"라는 객체로 받아온 후 "span" Tag 인 것을 "spans" 리스트에 추가
-for page in pages:
-  spans.append(page.find("span"))
+# "links" 리스트에 저장된 값을 차례대로 "link"라는 객체로 받아온 후 "span" Tag 인 것을 "pages" 리스트에 추가
+# [:-1] = "links" 리스트에 저장된 값 중 마지막 값을 제외한다는 뜻
+for link in links[:-1]:
+  # pages.append(link.find("span").string)
+  # anchor Tag의 String을 가지고 와도 span의 String을 가지고 온다
+  pages.append(int(link.string)) 
 
-# # "spans" 리스트에 저장된 값 중 마지막에서 1번째 item
-# print(spans[-1])
+# # "pages" 리스트에 저장된 값 중 마지막에서 1번째 item
+# print(pages[-1])
 
-# "spans" 리스트에 저장된 값 중 마지막 값을 제외하고 가져온 후 다시 spans에 넣기
-spans = spans[:-1]
+# "pages"에 저장된 값 중 가장 큰 값(마지막 값)만 가져오기
+max_page = pages[-1]
